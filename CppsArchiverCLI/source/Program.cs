4c5f1784bbfdf166ebf353d2e87485e8074ac100
Arkadiy Zakharov <wirex2000@gmail.com>
2024-07-21 20:03:12 +0500

    Created cs projects and sln

---
+++ CppsArchiverCLI/source/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CppsArchiverCLI.source
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
