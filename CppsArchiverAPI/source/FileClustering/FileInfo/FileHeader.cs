b5959862ca69a9e8c3545164ce8f63f0c47251a7
Arkadiy Zakharov <wirex2000@gmail.com>
2024-08-02 21:15:42 +0500

    pc move

---
+++ CppsArchiverAPI/source/FileClustering/FileInfo/FileHeader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppsArchiverAPI.source.FileClustering
+{
+	internal class FileHeader(int signature, short minVersion, short general, short compression, short lastModTime, short lastModDate, int crcUncompressedData, int compressedSize, int uncompressedSize, short fileNameLength, short extraFieldLength, string fileName, string extraField)
+	{
+		public int Signature { get; } = signature;
+		public short MinVersion { get; } = minVersion;
+		public short General { get; } = general;
+		public short Compression { get; } = compression;
+		public short LastModTime { get; } = lastModTime;
+		public short LastModDate { get; } = lastModDate;
+		public int CrcUncompressedData { get; } = crcUncompressedData;
+		public int CompressedSize { get; } = compressedSize;
+		public int UncompressedSize { get; } = uncompressedSize;
+		public short FileNameLength { get; } = fileNameLength;
+		public short ExtraFieldLength { get; } = extraFieldLength;
+		public string FileName { get; } = fileName;
+		public string ExtraField { get; } = extraField;
+	}
+}
