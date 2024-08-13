2830ac9ecf4439b88050d52fe7ebcac60b2db74e
Arkadiy Zakharov <wirex2000@gmail.com>
2024-08-14 00:09:11 +0500

    made EOCD deserializer

--- CppsArchiverAPI/source/FileClustering/FileManager.cs
+++ CppsArchiverAPI/source/FileClustering/FileManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,18 +20,25 @@ namespace CppsArchiverAPI.FileClustering
 		private static EOCDHeader? FindCentralDirectory(Stream stream)
 		{
 			long pos = stream.Length - 22;
-			char[] cbuffer = new char[4];
+			char[] buffer = new char[4];
 			using StreamReader sr = new(stream, leaveOpen: true);
 
 			while (true)
 			{
 				stream.Position = pos;
-				sr.Read(cbuffer, 0, cbuffer.Length);
+				sr.Read(buffer, 0, buffer.Length);
+				string testString = new(buffer);
 
-				if (new string(cbuffer) == "PK\u0005\u0006")
+				if (testString == "PK\u0005\u0006")
 				{
+					stream.Position = pos + 4;
+					//byte[] bytes = new byte[stream.Length - stream.Position];
+					//stream.Read(bytes, 0, bytes.Length);
+
 					return DeserializeEOCDHeader(stream);
 				}
+
+				//TODO: PK\u0006\u0006
 
 				pos--;
 				if (pos < 0)
@@ -48,7 +56,42 @@ namespace CppsArchiverAPI.FileClustering
 
 		private static EOCDHeader DeserializeEOCDHeader(Stream stream)
 		{
-			return null!;
+			short diskNo = DeserializeShort(stream);
+			short diskStart = DeserializeShort(stream);
+			short currentRecords = DeserializeShort(stream);
+			short totalRecords = DeserializeShort(stream);
+
+			int size = DeserializeInt(stream);
+			int offset = DeserializeInt(stream);
+
+			short commentSize = DeserializeShort(stream);
+
+			char[] cbuffer = new char[commentSize];
+
+			StreamReader sr = new(stream, leaveOpen: true);
+			sr.Read(cbuffer, 0, cbuffer.Length);
+			sr.Close();
+
+			string comment = new(cbuffer);
+
+			return new(diskNo, diskStart, currentRecords, totalRecords, size, offset, commentSize, comment);
+		}
+
+		private static short DeserializeShort(Stream stream)
+		{
+			byte[] shortBuffer = new byte[2];
+
+			stream.Read(shortBuffer, 0, 2);
+			//TODO: add little-endian check
+			return BitConverter.ToInt16(shortBuffer, 0);
+		}
+		private static int DeserializeInt(Stream stream)
+		{
+			byte[] intBuffer = new byte[4];
+
+			stream.Read(intBuffer, 0, 4);
+			//TODO: add little-endian check
+			return BitConverter.ToInt32(intBuffer, 0);
 		}
 	}
 }
