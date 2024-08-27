561e758cbd129e96d4e5a32f0936221ca67b9070
Arkadiy Zakharov <wirex2000@gmail.com>
2024-08-27 18:10:47 +0500

    refactor

---
+++ CppsArchiverAPI/source/LibraryImport/BaseProcessHandler.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CppsArchiverAPI.LibraryImport
+{
+	public abstract partial class BaseProcessHandler(Stream sourceStream, Stream destStream, short compression, ulong size)
+	{
+
+		#region delegates
+
+		private delegate void SendDataCallback(IntPtr data, ulong dataSize);
+
+		private delegate IntPtr ReceiveDataCallback(ulong size);
+
+		private delegate void FinishCallback();
+
+		#endregion
+
+		#region private_unsafe 
+
+		[LibraryImport("ArchiverImplementation", EntryPoint = "getExceptionMessage", StringMarshalling = StringMarshalling.Utf8)]
+		private static unsafe partial string GetExceptionMessage(void* exception);
+
+		[LibraryImport("ArchiverImplementation", EntryPoint = "deleteException")]
+		private static unsafe partial void DeleteException(void* exception);
+
+		[LibraryImport("ArchiverImplementation", EntryPoint = "setBufferSize")]
+		private static partial void SetBufferSize(ulong size);
+
+		#endregion
+
+		private readonly Stream sourceStream = sourceStream;
+		private readonly Stream destStream = destStream;
+
+		private readonly short compression = compression;
+		private readonly ulong size = size;
+
+		private readonly List<GCHandle> gCHandles = [];
+
+		private void Receive(IntPtr data, ulong dataSize)
+		{
+			byte[] buffer = new byte[dataSize];
+			Marshal.Copy(data, buffer, 0, buffer.Length);
+
+			destStream.Write(buffer);
+		}
+
+		private IntPtr Throw(ulong size)
+		{
+			byte[] buffer = new byte[size];
+			sourceStream.Read(buffer);
+
+			GCHandle pinnedArray = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+			IntPtr pointer = pinnedArray.AddrOfPinnedObject();
+
+			gCHandles.Add(pinnedArray);
+
+			return pointer;
+		}
+
+		private void Finish()
+		{
+			foreach (GCHandle h in gCHandles)
+			{
+				h.Free();
+			}
+		}
+
+		protected abstract unsafe void Process(ulong compressionMethod,
+			ulong size,
+			IntPtr sendCallback,
+			IntPtr receiveCallback,
+			IntPtr finishCallback,
+			ref void* exception);
+
+		public void ProcessFile()
+		{
+			unsafe
+			{
+				void* exception = null;
+				Process(
+					(ulong)compression,
+					size,
+					Marshal.GetFunctionPointerForDelegate((SendDataCallback)Receive),
+					Marshal.GetFunctionPointerForDelegate((ReceiveDataCallback)Throw),
+					Marshal.GetFunctionPointerForDelegate((FinishCallback)Finish),
+					ref exception);
+				if (exception != null)
+				{
+					string message = GetExceptionMessage(exception);
+					DeleteException(exception);
+
+					throw new(message);
+				}
+			}
+		}
+	}
+}
---
+++ CppsArchiverAPI/source/LibraryImport/DerivedProcessHandlers/UnzipHandler.cs
@@ -0,0 +1,31 @@
+﻿using CppsArchiverAPI.LibraryImport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CppsArchiverAPI.source.LibraryImport.DerivedProcessHandlers
+{
+	internal partial class UnzipHandler(Stream sourceStream, Stream destStream, short compression, ulong size) : BaseProcessHandler(sourceStream, destStream, compression, size)
+	{
+		#region private_unsafe
+
+		[LibraryImport("ArchiverImplementation", EntryPoint = "unzip")]
+		private static unsafe partial void Unzip(
+			ulong compressionMethod,
+			ulong compressedSize,
+			IntPtr sendCallback,
+			IntPtr receiveCallback,
+			IntPtr finishCallback,
+			ref void* exception);
+		#endregion
+
+		protected override unsafe void Process(ulong compressionMethod, ulong size, nint sendCallback, nint receiveCallback, nint finishCallback, ref void* exception)
+		{
+			Unzip(compressionMethod, size, sendCallback, receiveCallback, finishCallback, ref exception);
+		}
+
+	}
+}
